4cbc5f680481fd91775a0f4381ce53a67a05dea3
HyperActiveJ <6298301+HyperActiveJ@users.noreply.github.com>
2022-04-24 10:58:03 -0700

    Move nested_dict

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lennoxs30api",
-    version="0.9.1",
+    version="0.9.2",
     description="API Wrapper for Lennox S30 Cloud and LAN API",
     long_description=long_description,
     long_description_content_type="text/markdown",
