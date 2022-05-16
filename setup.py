f3e6e5db67480aa072a5f75c4f5496c90dfe6d9b
PeteRager <76050312+PeteRager@users.noreply.github.com>
2022-05-16 08:20:04 -0400

    Version to 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lennoxs30api",
-    version="0.1.7",
+    version="0.1.8",
     description="API Wrapper for Lennox S30 Cloud and LAN API",
     long_description=long_description,
     long_description_content_type="text/markdown",
