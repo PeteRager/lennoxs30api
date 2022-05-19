d86506174df87a2815e6d850668b838aa94d47dd
PeteRager <76050312+PeteRager@users.noreply.github.com>
2022-05-19 17:46:53 -0400

    Bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lennoxs30api",
-    version="0.1.8",
+    version="0.1.9",
     description="API Wrapper for Lennox S30 Cloud and LAN API",
     long_description=long_description,
     long_description_content_type="text/markdown",
