ce23ab57eeef9a4511c6c5571cabaf24a4f77937
PeteRager <76050312+PeteRager@users.noreply.github.com>
2025-05-09 11:10:59 -0400

    Version to 0.2.18

--- lennoxs30api/__init__.py
+++ lennoxs30api/__init__.py
@@ -1,5 +1,5 @@
 """Lennoxs30api"""
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 from .lennox_home import *
 from .lennox_period import *
 from .lennox_schedule import *
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lennoxs30api",
-    version="0.2.17",
+    version="0.2.18",
     description="API Wrapper for Lennox S30 Cloud and LAN API",
     long_description=long_description,
     long_description_content_type="text/markdown",
